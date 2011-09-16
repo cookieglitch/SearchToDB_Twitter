01ce294f346b0dca48475839d825ba6e9e36c847
John Tiernan <jtiernan@drownedmusic.co.uk>
2011-09-16 15:00:41 +0100

    Basic format

---
+++ src/SearchToDB_Twitter.java
@@ -0,0 +1,23 @@
+/**
+ * Twitter Search Exporter
+ * 
+ * @author John Tiernan
+ * @version "%I%, %G%"
+ */
+
+import twitter4j.*;
+
+public class SearchToDB_Twitter {
+
+	/**
+	 * @param args[0] Search keyword
+	 * @param args[1] Username
+	 * @param args[2] Password
+	 * @param args[3] Destination
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
