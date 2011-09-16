aef3d3b2e8849da4cb3685b49f20952d95ff795e
John Tiernan <jtiernan@drownedmusic.co.uk>
2011-09-16 15:43:09 +0100

    Added Query sample from lib docs

--- src/SearchToDB_Twitter.java
+++ src/SearchToDB_Twitter.java
@@ -2,21 +2,48 @@
  * Twitter Search Exporter
  * 
  * @author John Tiernan
- * @version "%I%, %G%"
+ * @version 2011_16/Sept
  */
 
 import twitter4j.*;
+import java.util.List;
 
 public class SearchToDB_Twitter {
 
 	/**
 	 * @param args[0] Search keyword
-	 * @param args[1] Username
+	 * @param args[1] Twitter User name
 	 * @param args[2] Password
 	 * @param args[3] Destination
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		if (args.length < 1) {
+            System.out.println("java -jar SearchToDB_Twitter.jar \"[query]\"");
+            System.exit(-1);
+        }
+		
+		//Command line args
+		String keyword = args[0];
+		//String uname = args[1];
+		//String pass = args[2];
+		//String dest = args[3];
+		
+		//Create Twitter4j object
+		Twitter twit = new TwitterFactory().getInstance();
+		
+		//Example code from lib
+		try {
+            QueryResult result = twit.search(new Query(args[0]));
+            List<Tweet> tweets = result.getTweets();
+            for (Tweet tweet : tweets) {
+                System.out.println("@" + tweet.getFromUser() + " - " + tweet.getText());
+            }
+            System.exit(0);
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Failed to search tweets: " + te.getMessage());
+            System.exit(-1);
+        }
 		
 	}
 
